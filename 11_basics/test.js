8c7b73bbc6d17870b805b1d4cddab300c721a873
Sukrit Majumdar <104047395+sukritmajumdar-jgec6009@users.noreply.github.com>
2025-07-02 08:40:15 +0000

    continued

--- 11_basics/test.js
+++ 11_basics/test.js
@@ -1,1 +1,9 @@
-console.log("sukrit")
+const name1="Sukrit"
+var Phone=7811808906
+address="kharagpur"
+//console.table([name1,Phone,address])
+console.log(typeof Phone)
+apnum=33
+let capnum=String(apnum)
+console.log(capnum)
+console.log(typeof capnum)
