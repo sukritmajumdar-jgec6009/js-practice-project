e0073445f32b3ad5481b28aea779ade48fded609
Sukrit Majumdar <104047395+sukritmajumdar-jgec6009@users.noreply.github.com>
2025-07-03 08:31:19 +0000

    ccv

--- 11_basics/test.js
+++ 11_basics/test.js
@@ -7,3 +7,16 @@ apnum=33
 let capnum=String(apnum)
 console.log(capnum)
 console.log(typeof capnum)
+let c="ggu"
+let d=Boolean(c)
+let e=Number(d)
+console.log(e);
+let gc=9
+let gcc=9.0
+let bbv;
+console.log(null<=0);
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined<=0);
+console.log(undefined==0)
+console.log(typeof bbv +" " + typeof gcc)
